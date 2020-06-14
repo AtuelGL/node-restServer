8f2eebedc2d2b6438ebf8e22505a26a2aafe45ee
AtuelGL <atuelgl@gmail.com>
2020-06-14 20:45:33 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://AtuelGL:hp8Ha42q97YHb9o8@cluster0-mgtwl.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
